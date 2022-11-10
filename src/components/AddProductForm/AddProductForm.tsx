14bea50499a3f8ad6c6fa634da973c958d266460
Sasha Shevchenko <sasha.shevchenko763@gmail.com>
2022-11-10 15:55:25 +0200

    Solution

--- src/components/AddProductForm/AddProductForm.tsx
+++ src/components/AddProductForm/AddProductForm.tsx
@@ -1,1 +1,48 @@
-export const AddProductForm = () => {};
+type Props = {
+title: string;
+setQuere: (value: string) => void;
+setSelect: (value: string) => void;
+reset: () => void;
+};
+
+export const AddProductForm: React.FC<Props> = ({title, setQuere, setSelect,reset}) => (
+  <form className="form">
+  <div className="field">
+    <div className="control">
+      <input
+        className="input"
+        type="text"
+        placeholder="product name"
+        value={title}
+        onChange={(event) => setQuere(event.target.value)}
+      />
+    </div>
+  </div>
+
+  <div className="field">
+    <div className="control">
+      <div className="select">
+        <select onChange={(event) => setSelect(event.target.value)}>
+          <option value={'Grocery'}>Grocery</option>
+          <option value={'Drinks'}>Drinks</option>
+          <option value={'Fruits'}>Fruits</option>
+          <option value={'Electronics'}>Electronics</option>
+          <option value={'Clothes'}>Clothes</option>
+        </select>
+      </div>
+    </div>
+  </div>
+
+  <div className="field is-grouped">
+    <div className="control">
+      <button
+        type="submit"
+        className="button is-link"
+        onClick={reset}
+      >
+        Submit
+      </button>
+    </div>
+  </div>
+</form>
+);

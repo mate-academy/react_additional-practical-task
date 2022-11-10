8ab899ac526641a693024efdf76b6ea41ffab904
Vadym Mamchur <vmchr344@gmail.com>
2022-11-10 15:39:27 +0200

    practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { ProductWithCategory } from './types/ProductWithCategory';
 
 import productsFromServer from './api/products';
 import categoriesFromServer from './api/categories';
+import { ProductTable } from './components/ProductTable';
+import { AddProductForm } from './components/AddProductForm';
 
 const findCategoryById = (categoryId: number) => {
   const foundCategory = categoriesFromServer.find(category => (
@@ -21,88 +23,36 @@ const productsWithCategories: ProductWithCategory[] = productsFromServer.map(
 );
 
 export const App: React.FC = () => {
+  const [products, setProducts] = useState<ProductWithCategory[]>(productsWithCategories);
+
+  const handleAddNewProduct = (product: any) => {
+    setProducts(currentProducts => [...currentProducts, product]);
+  }
+
+  const generateProductId = () => {
+    const productsIds = products.map(product => product.id);
+
+    return Math.max(...productsIds) + 1;
+  }
+
+  const generateCategoryId = () => {
+    const categoriesIds = products.map(product => product.categoryId);
+
+    return Math.max(...categoriesIds) + 1;
+  }
+
   return (
     <div className="section">
       <div className="container">
         <h1 className="title">Product Categories</h1>
 
-        <form className="form">
-          <div className="field">
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                placeholder="product name"
-              />
-            </div>
-          </div>
+        <AddProductForm
+          onAddNewProduct={handleAddNewProduct}
+          generateProductId={generateProductId}
+          generateCategoryId={generateCategoryId}
+        />
 
-          <div className="field">
-            <div className="control">
-              <div className="select">
-                <select>
-                  <option>Grocery</option>
-                  <option>Drinks</option>
-                  <option>Fruits</option>
-                  <option>Electronics</option>
-                  <option>Clothes</option>
-                </select>
-              </div>
-            </div>
-          </div>
-
-          <div className="field is-grouped">
-            <div className="control">
-              <button
-                type="submit"
-                className="button is-link"
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
-
-        <table
-          className="table is-striped is-narrow is-fullwidth"
-        >
-          <thead>
-            <tr>
-              <th>
-                <span className="is-flex is-flex-wrap-nowrap">
-                  ID
-                </span>
-              </th>
-
-              <th>
-                <span className="is-flex is-flex-wrap-nowrap">
-                  Product
-                </span>
-              </th>
-
-              <th>
-                <span className="is-flex is-flex-wrap-nowrap">
-                  Category
-                </span>
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {productsWithCategories.map(product => (
-              <tr key={product.id}>
-                <td className="has-text-weight-bold">
-                  {product.id}
-                </td>
-                <td>{product.name}</td>
-
-                {product.category?.title && (
-                  <td>{product.category?.title}</td>
-                )}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <ProductTable products={products} />
       </div>
     </div>
   );
--- src/components/AddProductForm/AddProductForm.tsx
+++ src/components/AddProductForm/AddProductForm.tsx
@@ -1,1 +1,74 @@
-export const AddProductForm = () => {};
+import React, { useState } from 'react';
+
+import { ProductWithCategory } from '../../types/ProductWithCategory';
+
+interface Props {
+  onAddNewProduct: (product: ProductWithCategory) => void;
+  generateProductId: () => number;
+  generateCategoryId: () => number;
+}
+
+export const AddProductForm: React.FC<Props> = ({ onAddNewProduct, generateProductId, generateCategoryId }) => {
+  const [productName, setProductName] = useState('');
+  const [category, setCategory] = useState('');
+
+  const submitProductForm = () => {
+    const productId = generateProductId();
+    const categoryId = generateCategoryId();
+
+    const newProduct = {
+      id: productId,
+      name: productName,
+      categoryId,
+      category,
+    }
+
+    onAddNewProduct({newProduct})
+  }
+
+  return (
+    <form 
+      className="form"
+      onSubmit={submitProductForm}
+    >
+      <div className="field">
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="product name"
+            value={productName}
+            onChange={event => setProductName(event.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <div className="select">
+            <select
+              onChange={event => setCategory(event.target.value)}
+            >
+              <option>Grocery</option>
+              <option>Drinks</option>
+              <option>Fruits</option>
+              <option>Electronics</option>
+              <option>Clothes</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="field is-grouped">
+        <div className="control">
+          <button
+            type="submit"
+            className="button is-link"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+};

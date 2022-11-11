5afcab69bfab8e711609af5c02713927779d00f8
Arsen-Shpak <a.shpakovich2000@gmail.com>
2022-11-11 13:56:59 +0200

    additional-practise

--- src/components/AddProductForm/AddProductForm.tsx
+++ src/components/AddProductForm/AddProductForm.tsx
@@ -1,1 +1,61 @@
-export const AddProductForm = () => {};
+import React from 'react';
+
+type Props = {
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+  setField: React.Dispatch<React.SetStateAction<string>>;
+  handleSubmit: () => void;
+  query: string;
+  field: string;
+};
+export const AddProductForm: React.FC<Props> = ({
+  setQuery,
+  setField,
+  handleSubmit,
+  query,
+  field,
+}) => {
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="field">
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="product name"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <div className="select">
+            <select
+              value={field}
+              onChange={(event) => setField(event.target.value)}
+            >
+              {/* {categoriesFromServer.map((categoria) => (
+                <option>{categoria.title}</option>
+              ))} */}
+              <option>Grocery</option>
+              <option>Drinks</option>
+              <option>Fruits</option>
+              <option>Electronics</option>
+              <option>Clothes</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="field is-grouped">
+        <div className="control">
+          <button type="submit" className="button is-link">
+            Submit
+          </button>
+
+        </div>
+      </div>
+    </form>
+  );
+};
--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -1,1 +1,44 @@
-export const ProductTable = () => {};
+import React from 'react';
+import { ProductWithCategory } from '../../types/ProductWithCategory';
+type Props = {
+  products: ProductWithCategory[];
+  DeleteProduct: (deleteID: number) => void;
+};
+
+export const ProductTable: React.FC<Props> = ({ products, DeleteProduct }) => {
+  return (
+    <table className="table is-striped is-narrow is-fullwidth">
+      <thead>
+        <tr>
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">ID</span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">Product</span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">Category</span>
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id}>
+            <td className="has-text-weight-bold">{product.id}</td>
+            <td>{product.name}</td>
+
+            {product.category?.title && <td>{product.category?.title}</td>}
+            <td>
+              <button className="button is-link" onClick={DeleteProduct(product.id)}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};

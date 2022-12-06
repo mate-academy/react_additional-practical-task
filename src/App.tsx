63790170ea8e8003b0fd0903e6d43d2e9dbbb387
Anastasiia Smirnova <anastasia.smirnova96@gmail.com>
2022-12-06 17:09:42 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { ProductWithCategory } from './types/ProductWithCategory';
 import productsFromServer from './api/products';
 import categoriesFromServer from './api/categories';
 
+import { ProductTable } from './components/ProductTable';
+import { AddProductForm } from './components/AddProductForm';
+
 const findCategoryById = (categoryId: number) => {
   const foundCategory = categoriesFromServer.find(category => (
     category.id === categoryId
@@ -20,89 +23,17 @@ const productsWithCategories: ProductWithCategory[] = productsFromServer.map(
   }),
 );
 
+const visibleProducts = [...productsWithCategories];
+
 export const App: React.FC = () => {
   return (
     <div className="section">
       <div className="container">
         <h1 className="title">Product Categories</h1>
 
-        <form className="form">
-          <div className="field">
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                placeholder="product name"
-              />
-            </div>
-          </div>
+        <AddProductForm visibleProducts={visibleProducts} categories={categoriesFromServer}/>
 
-          <div className="field">
-            <div className="control">
-              <div className="select">
-                <select>
-                  <option>Grocery</option>
-                  <option>Drinks</option>
-                  <option>Fruits</option>
-                  <option>Electronics</option>
-                  <option>Clothes</option>
-                </select>
-              </div>
-            </div>
-          </div>
-
-          <div className="field is-grouped">
-            <div className="control">
-              <button
-                type="submit"
-                className="button is-link"
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
-
-        <table
-          className="table is-striped is-narrow is-fullwidth"
-        >
-          <thead>
-            <tr>
-              <th>
-                <span className="is-flex is-flex-wrap-nowrap">
-                  ID
-                </span>
-              </th>
-
-              <th>
-                <span className="is-flex is-flex-wrap-nowrap">
-                  Product
-                </span>
-              </th>
-
-              <th>
-                <span className="is-flex is-flex-wrap-nowrap">
-                  Category
-                </span>
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {productsWithCategories.map(product => (
-              <tr key={product.id}>
-                <td className="has-text-weight-bold">
-                  {product.id}
-                </td>
-                <td>{product.name}</td>
-
-                {product.category?.title && (
-                  <td>{product.category?.title}</td>
-                )}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <ProductTable productsWithCategories={productsWithCategories} />
       </div>
     </div>
   );
--- src/components/AddProductForm/AddProductForm.tsx
+++ src/components/AddProductForm/AddProductForm.tsx
@@ -1,1 +1,46 @@
-export const AddProductForm = () => {};
+import React, { useState } from 'react';
+import categories from '../../api/categories';
+
+export const AddProductForm: React.FC = ({ visibleProducts }) => {
+  const [query, setQuery] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  
+  if (query.length > 0 && selectedCategory) {
+    
+  }
+
+  return (
+    <form className="form">
+      <div className="field">
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="product name"
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <div className="select">
+            <select>
+              {categories.map(category => <option>{category.title}</option>)}
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="field is-grouped">
+        <div className="control">
+          <button
+            type="submit"
+            className="button is-link"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+};

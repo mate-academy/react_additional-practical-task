e7d66e8d160a17227ec3e15b22d662acb959afd2
Karyna Salnykova <109825783+karyna-salnykova@users.noreply.github.com>
2022-12-07 14:17:35 +0200

    First 2 tasks was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,20 @@
 import React from 'react';
 import './App.scss';
-import { ProductWithCategory } from './types/ProductWithCategory';
 
 import productsFromServer from './api/products';
 import categoriesFromServer from './api/categories';
 
-const findCategoryById = (categoryId: number) => {
-  const foundCategory = categoriesFromServer.find(category => (
-    category.id === categoryId
-  ));
-
-  return foundCategory || null;
-};
-
-const productsWithCategories: ProductWithCategory[] = productsFromServer.map(
-  (product) => ({
-    ...product,
-    category: findCategoryById(product.categoryId),
-  }),
-);
+import { ProductTable } from './components/ProductTable';
+import { AddProductForm } from './components/AddProductForm';
 
 export const App: React.FC = () => {
   return (
     <div className="section">
       <div className="container">
         <h1 className="title">Product Categories</h1>
+        <AddProductForm categories={categoriesFromServer}/>
 
-        <form className="form">
-          <div className="field">
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                placeholder="product name"
-              />
-            </div>
-          </div>
-
-          <div className="field">
-            <div className="control">
-              <div className="select">
-                <select>
-                  <option>Grocery</option>
-                  <option>Drinks</option>
-                  <option>Fruits</option>
-                  <option>Electronics</option>
-                  <option>Clothes</option>
-                </select>
-              </div>
-            </div>
-          </div>
-
-          <div className="field is-grouped">
-            <div className="control">
-              <button
-                type="submit"
-                className="button is-link"
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
-
-        <table
-          className="table is-striped is-narrow is-fullwidth"
-        >
-          <thead>
-            <tr>
-              <th>
-                <span className="is-flex is-flex-wrap-nowrap">
-                  ID
-                </span>
-              </th>
-
-              <th>
-                <span className="is-flex is-flex-wrap-nowrap">
-                  Product
-                </span>
-              </th>
-
-              <th>
-                <span className="is-flex is-flex-wrap-nowrap">
-                  Category
-                </span>
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {productsWithCategories.map(product => (
-              <tr key={product.id}>
-                <td className="has-text-weight-bold">
-                  {product.id}
-                </td>
-                <td>{product.name}</td>
-
-                {product.category?.title && (
-                  <td>{product.category?.title}</td>
-                )}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <ProductTable products={productsFromServer}/>
       </div>
     </div>
   );
--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -1,1 +1,72 @@
-export const ProductTable = () => {};
+import React from 'react';
+
+import { Product } from '../../types/Product';
+import { Category } from '../../types/Category';
+import { ProductWithCategory } from '../../types/ProductWithCategory';
+
+import productsFromServer from '../../api/products';
+import categoriesFromServer from '../../api/categories';
+
+type Props = {
+  products: Product[],
+};
+
+const findCategoryById = (categoryId: number) => {
+  const foundCategory = categoriesFromServer.find((category: Category) => (
+    category.id === categoryId
+  ));
+
+  return foundCategory || null;
+};
+
+const productsWithCategories: ProductWithCategory[] = productsFromServer.map(
+  (product: Product) => ({
+    ...product,
+    category: findCategoryById(product.categoryId),
+  }),
+);
+
+export const ProductTable: React.FC<Props> = () => {
+  return (
+    <table
+      className="table is-striped is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              ID
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Product
+            </span>
+          </th>
+
+          <th>
+            <span className="is-flex is-flex-wrap-nowrap">
+              Category
+            </span>
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {productsWithCategories.map(product => (
+          <tr key={product.id}>
+            <td className="has-text-weight-bold">
+              {product.id}
+            </td>
+            <td>{product.name}</td>
+
+            {product.category?.title && (
+              <td>{product.category?.title}</td>
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+};

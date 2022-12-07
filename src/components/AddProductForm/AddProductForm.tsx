f3e614f90f7a2d09da23f30b218b1e58a6ed3f00
AndreyChernysh <achernysh744@gmail.com>
2022-12-07 17:33:15 +0200

    try

--- src/components/AddProductForm/AddProductForm.tsx
+++ src/components/AddProductForm/AddProductForm.tsx
@@ -1,1 +1,57 @@
-export const AddProductForm = () => {};
+import React, { ChangeEvent, useState } from "react";
+import { Category } from "../../types/Category";
+import categoriesFromServer from '../../api/categories';
+
+type Props = {
+  categories: Category[];
+}
+
+export const AddProductForm: React.FC<Props> = () => {
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('');
+  return (
+    <form className="form">
+      <div className="field">
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="product name"
+            value={name}
+            onChange={(event) => (
+              setName(event.target.value)
+            )}
+          />
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <div className="select">
+            <select
+              value={category}
+              onChange={(event: ChangeEvent<HTMLSelectElement>) => (setCategory(event.target.value))}
+            >
+              {categoriesFromServer.map(category =>
+                <option value={category.title} key={category.id}>
+                  {category.title}
+                </option>
+              )}
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="field is-grouped">
+        <div className="control">
+          <button
+            type="submit"
+            className="button is-link"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+};
--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -1,1 +1,70 @@
-export const ProductTable = () => {};
+import React from "react";
+import { Product } from "../../types/Product";
+import productsFromServer from '../../api/products';
+import categoriesFromServer from '../../api/categories';
+import { ProductWithCategory } from '../../types/ProductWithCategory';
+
+type Props = {
+  products: Product[],
+}
+
+const findCategoryById = (categoryId: number) => {
+  const foundCategory = categoriesFromServer.find(category => (
+    category.id === categoryId
+  ));
+
+  return foundCategory || null;
+};
+
+export const productsWithCategories: ProductWithCategory[] = productsFromServer.map(
+  (product) => ({
+    ...product,
+    category: findCategoryById(product.categoryId),
+  }),
+);
+
+export const ProductTable: React.FC<Props> = () => {
+
+  return (
+    <table
+    className="table is-striped is-narrow is-fullwidth"
+  >
+    <thead>
+      <tr>
+        <th>
+          <span className="is-flex is-flex-wrap-nowrap">
+            ID
+          </span>
+        </th>
+
+        <th>
+          <span className="is-flex is-flex-wrap-nowrap">
+            Product
+          </span>
+        </th>
+
+        <th>
+          <span className="is-flex is-flex-wrap-nowrap">
+            Category
+          </span>
+        </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {productsWithCategories.map(product => (
+        <tr key={product.id}>
+          <td className="has-text-weight-bold">
+            {product.id}
+          </td>
+          <td>{product.name}</td>
+
+          {product.category?.title && (
+            <td>{`${product.category?.title} ${product.category?.icon}`}</td>
+          )}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+  )
+};
